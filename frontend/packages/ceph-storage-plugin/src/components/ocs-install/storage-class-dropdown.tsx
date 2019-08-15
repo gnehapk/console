bae9ba242d9cacf25ad2dbaccbe5c50032168919
Neha <gnehapk@gmail.com>
2019-08-15 14:13:41 +0900

    Create OCS Service workflow

---
+++ frontend/packages/ceph-storage-plugin/src/components/ocs-install/storage-class-dropdown.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as _ from 'lodash';
+import { Firehose } from '@console/internal/components/utils';
+import { StorageClassDropdown_ as SCDropdown } from '@console/internal/components/utils/storage-class-dropdown';
+import { K8sResourceKind } from '@console/internal/module/k8s';
+
+const cephStorageProvisioners = ['ceph.rook.io/block', 'cephfs.csi.ceph.com', 'rbd.csi.ceph.com'];
+
+export const OCSStorageClassDropdown = (props) => (
+  <Firehose resources={[{ kind: 'StorageClass', prop: 'StorageClass', isList: true }]}>
+    <StorageClassDropdown {...props} />
+  </Firehose>
+);
+
+const StorageClassDropdown = (props) => {
+  const getFilteredStorageClassData = (storageClassData: K8sResourceKind[]): K8sResourceKind[] =>
+    storageClassData.filter(
+      (sc: K8sResourceKind) => !cephStorageProvisioners.includes(_.get(sc, 'provisioner')),
+    );
+
+  const scConfig = _.cloneDeep(props);
+  const scLoaded = _.get(scConfig.resources.StorageClass, 'loaded');
+  const scData = _.get(scConfig.resources.StorageClass, 'data', []) as K8sResourceKind[];
+  const filteredSCData = getFilteredStorageClassData(scData);
+
+  if (scLoaded) {
+    scConfig.resources.StorageClass.data = filteredSCData;
+  }
+
+  return <SCDropdown {...scConfig} />;
+};
